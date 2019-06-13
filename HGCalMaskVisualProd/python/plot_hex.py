158b7b6da2b00153c6b22791d0cd0c47e90c3501
bfontana <bruno.alves@cern.ch>
2019-06-13 12:00:33 +0200

    remove data/ folder dependence

--- HGCalMaskVisualProd/python/plot_hex.py
+++ HGCalMaskVisualProd/python/plot_hex.py
@@ -1,94 +1,23 @@
-import sys
-import os
-
-import ROOT
+import sys, os
 import numpy as np
-from matplotlib import pyplot as plt
 import argparse
 import math, copy
+from matplotlib import pyplot as plt
 
-import RootUtils
-import SystemUtils
-import Argparser
+from ROOT import TCanvas, TFile, TIter, TH2Poly, TPaveLabel, TKey
+from ROOT import gDirectory, gROOT, gStyle, kTRUE, kFALSE
+gROOT.SetBatch(kTRUE)
+gStyle.SetOptStat(kFALSE)
+
+import RootUtils, SystemUtils, Argparser
+
+parser = argparse.ArgumentParser()
+FLAGS, _ = Argparser.add_args(parser)
+Argparser.print_args(FLAGS)
 
 def getUVFromTitle(title):
     u, v, N, rest = title.split(',')
     return u, v, N, rest
-
-def convertToHex(h, N):
-    """
-    Performs the conversion between standard (U,V) TH2D to
-    (X,Y) hexagonal histogram within an hexagonal grid.
-    """
-    def addBins(h, xstart, ystart, a, k, s):
-        from array import array
-        x = array('d', [0., 0., 0., 0., 0., 0.])
-        y = array('d', [0., 0., 0., 0., 0., 0.])
-        xloop = xstart
-        yloop = ystart + a*math.sqrt(3)/2.0;
-
-        for sCounter in range(s):
-            xtemp = xloop # Resets the temp variable
-            
-            #Determine the number of hexagons in that row
-            if sCounter%2 == 0:
-                numberOfHexagonsInTheRow = k
-            else:
-                numberOfHexagonsInTheRow = k - 1
- 
-            for kCounter in range(numberOfHexagonsInTheRow):
-                #Go around the hexagon
-                x[0] = xtemp
-                y[0] = yloop
-                x[1] = x[0] + a/2.
-                y[1] = y[0] + a*math.sqrt(3)/2.
-                x[2] = x[1] + a
-                y[2] = y[1]
-                x[3] = x[0] + 2*a
-                y[3] = y[0]
-                x[4] = x[2]
-                y[4] = y[3] - a*math.sqrt(3)/2.
-                x[5] = x[1]
-                y[5] = y[4] 
-
-                h.AddBin(6, x, y)
- 
-                # Go right
-                xtemp += 3*a
- 
-            # Increment the starting position
-            if sCounter%2 == 0:
-                xloop += 1.5*a
-            else:
-                xloop -= 1.5*a
-            yloop += a*math.sqrt(3)/2
-
-    def convertCoords(u, v):
-        return 1.5*(v-N)+1, u-0.5*(N+v)
-    
-    N2 = 2*N-1
-
-    x00, y00 = convertCoords(0, 0)
-    x01, y01 = convertCoords(0, 1)
-    dist_side = N * math.sqrt( (x01-x00)**2 + (y01-y00)**2 )
-    #bottom left graph corner
-    startx = - dist_side*math.sin(math.pi/3)
-    starty = y00 - dist_side*math.cos(math.pi/3)
-
-    _, miny = convertCoords(0, N-1)
-    _, maxy = convertCoords(N2, N-1)
-    maxx, _ = convertCoords(N2, N2)
-
-    hhex = ROOT.TH2Poly('hhex', 'hhex', startx-5, maxx, miny-5, maxy+15)
-    addBins(hhex,startx,starty,(x01-x00)/math.sqrt(3)+0.2,N,2*N2)
-
-    for u in range(h.GetNbinsX()):
-        for v in range(h.GetNbinsY()):
-            x, y = convertCoords(u, v)   
-            global_bin = hhex.FindBin(x, y)
-            hhex.SetBinContent(global_bin, h.GetBinContent(u, v))
-
-    return hhex
 
 def convertToHex_alternative(h, N, a):
     """
@@ -133,9 +62,12 @@ def convertToHex_alternative(h, N, a):
         y[5] = y[4] 
         hhex.AddBin(6, x, y)
  
-    hhex = ROOT.TH2Poly('hhex', 'hhex', -20, 20, -20, 20)
+    
+    lim = 20 if N==12 else 14
+    hhex = TH2Poly('hhex', 'hhex', -lim, lim, -lim, lim)
     xylist = []
     allWaf = allowedWafers()
+
     for U in range(h.GetNbinsX()-1):
         for V in range(h.GetNbinsY()-1):
             if (U,V) in allWaf:
@@ -149,60 +81,77 @@ def convertToHex_alternative(h, N, a):
             x, y = convertCoords(U, V)
             if (x,y) in xylist:  
                 global_bin = hhex.FindBin(x, y)
-                print("U:{} V:{} Bin content:{} ".format(U,V,h.GetBinContent(U, V)))
                 hhex.SetBinContent(global_bin, h.GetBinContent(U, V))
     return hhex
 
+def keyTitleMap(keylist):
+    """
+    Creates a map linking histogram titles to keys.
+    Arguments:
+    -> keylist: The list of keys from which the map is created
+    Returns:
+    -> a map
+    """
+    m = {}
+    for k in keylist:
+        if not gROOT.GetClass(k.GetClassName()).InheritsFrom('TH1'):
+            continue
+        m.update({k.ReadObj().GetTitle(): k})
+    return m
 
-parser = argparse.ArgumentParser()
-FLAGS, _ = Argparser.add_args(parser)
-Argparser.print_args(FLAGS)
 
-ROOT.gROOT.SetBatch(ROOT.kTRUE)
-ROOT.gStyle.SetOptStat(ROOT.kFALSE)
+pcoords = [[[0.02,0.49,0.98,0.93],    #pad1
+           [0.02,0.02,0.98,0.48]]]   #pad2
+pad_title = ['RecHits', 'Geom']
+for ic,_ in enumerate(pcoords):
+    if len(pcoords[ic]) != len(pad_title):
+        print(ic, len(pcoords[ic]), len(pad_title))
+        raise ValueError('The number of pads has to be consistent.')
+
+myfile = TFile.Open(FLAGS.root_file, "READ")
+online_dir = '/eos/user/b/bfontana/www/'
 maind = 'outpath'
 layers = [x for x in range(1,FLAGS.nlayers+1)]
 subd_names = ['layer'+str(layers[x]) for x in range(len(layers))]
-histo_names_rechits = [[] for _ in range(len(layers))]
-histo_names_geom = [[] for _ in range(len(layers))]
-for il,l in enumerate(layers):
-    with open('data/HistoNamesRecHitsLayer'+str(l)+'_mask'+str(FLAGS.mask)+'.txt') as f:
-        for line in f:
-            histo_names_rechits[il].append(line[:-1])
-    with open('data/HistoNamesGeomLayer'+str(l)+'_mask'+str(FLAGS.mask)+'.txt') as f:
-        for line in f:
-            histo_names_geom[il].append(line[:-1])
-
-myfile = ROOT.TFile.Open(FLAGS.root_file, "READ")
-online_dir = '/eos/user/b/bfontana/www/'
-pcoords = [[[0.02,0.49,0.98,0.93],    #pad1
-           [0.02,0.02,0.98,0.48]]]   #pad2
 for isd,sd in enumerate(subd_names):
     directory = os.path.join(online_dir,FLAGS.directory,sd)
     SystemUtils.createDir(os.path.join(online_dir,FLAGS.directory))
     SystemUtils.createDir(directory)
-    for nr,ng in zip(histo_names_rechits[isd],histo_names_geom[isd]):
-        with RootUtils.RootPlotting(ncanvas=1, npads=2, pcoords=pcoords) as plot:
-            #first pad
-            uvN1 = getUVFromTitle(nr)
-            h1 = myfile.Get(os.path.join(maind,sd,nr))
-            #h1 = convertToHex(h1, int(uvN1[2]))
-            h1 = convertToHex_alternative(h1, int(uvN1[2]), 0.5)
-            plot.plotHistogram(cpos=0, ppos=0, h=h1, title='RecHits', 
+    myfile.cd(os.path.join(maind,sd))
+    kmap = keyTitleMap(gDirectory.GetListOfKeys())
+    titleUsed = []
+    for htitle,key in kmap.items():
+        h = key.ReadObj()
+        uvN = getUVFromTitle(htitle)
+        title1 = uvN[0]+','+uvN[1]+','+uvN[2]+',RecHits'
+        title2 = uvN[0]+','+uvN[1]+','+uvN[2]+',Geom'
+        if title1 in titleUsed or title2 in titleUsed:
+            continue
+        titleUsed.append([title1, title2])
+        if htitle==title1:
+            histos = [h, kmap[title2].ReadObj()]
+        elif htitle==title2:
+            histos = [kmap[title1].ReadObj(), h]
+        else:
+            raise ValueError('There is a problem with the title of the histogram.')
+            
+        print(histos[0], histos[1])
+        with RootUtils.RootPlotting(ncanvas=1, npads=len(pad_title), 
+                                    pcoords=pcoords) as plot:
+            #for ipad in range(len(pad_title)):
+            ipad=0
+            h1 = convertToHex_alternative(histos[0], int(uvN[2]), 0.5)
+            plot.plotHistogram(cpos=0, ppos=ipad, h=h1, title=pad_title[ipad], 
                                xaxis_title='X [cm]', yaxis_title='Y [cm]')
-           
-            #second pad
-            uvN2 = getUVFromTitle(ng)
-            h2 = myfile.Get(os.path.join(maind,sd,ng))
-            #h2 = convertToHex(h2, int(uvN2[2]))
-            h2 = convertToHex_alternative(h2, int(uvN2[2]), 0.4474)
-            plot.plotHistogram(cpos=0, ppos=1, h=h2, title='Geometry', 
+
+            ipad=1
+            h2 = convertToHex_alternative(histos[1], int(uvN[2]), 0.5)
+            plot.plotHistogram(cpos=0, ppos=ipad, h=h2, title=pad_title[ipad], 
                                xaxis_title='X [cm]', yaxis_title='Y [cm]')
 
             #title and save
-            title = 'Layer #'+str(layers[isd])+'       Wafer: U='+uvN1[0]+', V='+uvN1[1] 
-            t = ROOT.TPaveLabel(0.02, 0.94, 0.98, 0.99, title)
+            ctitle = 'Layer #'+str(layers[isd])+'       Wafer: U='+uvN[0]+', V='+uvN[1] 
+            t = TPaveLabel(0.02, 0.94, 0.98, 0.99, ctitle)
             plot.setCanvasTitle(cpos=0, t=t)
-            name = directory +'/hex_'+uvN1[0]+'_'+uvN1[1]
-            plot.save(cpos=0, name=name)
-            quit()
+            picname = directory +'/hex_'+uvN[0]+'_'+uvN[1]
+            plot.save(cpos=0, name=picname)
