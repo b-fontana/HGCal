6c5349c9059acbf0c939c2f669664aac723bd3d1
bfontana <bruno.alves@cern.ch>
2019-07-05 12:43:45 +0200

    calibration restructuring

---
+++ HGCalMaskResolutionAna/python/Calibration.py
@@ -0,0 +1,126 @@
+import os
+import numpy as np
+from array import array as Carray
+from collections import OrderedDict
+
+from ROOT import TCanvas, TFile, TH2F, TLatex
+from UserCode.HGCalMaskResolutionAna.PartialWafersStudies import PartialWafersStudies
+from UserCode.HGCalMaskResolutionAna.RootTools import buildMedianProfile
+
+class Calibration(PartialWafersStudies, object):
+    def __init__(self, mingenen, mingeneta, maxgeneta, 
+                 plotLabel, outpath):
+        super(Calibration, self).__init__()
+        self.calib = OrderedDict({'L0': {}, 'L1': {}, 'L2': {}})
+        self.mingenen = mingenen
+        self.maxgeneta = maxgeneta
+        self.mingeneta = mingeneta
+        self.plotLabel = plotLabel
+        self.outpath = outpath
+
+    def _calibrateSpectrum(self, h, title, proc, func='pol1'):
+        """
+        Calibrates a DeltaE/E versus x spectrum based on the profile.
+        """
+        c = TCanvas('c','c',500,500)
+        c.SetTopMargin(0.05)
+        c.SetBottomMargin(0.1)
+        c.SetLeftMargin(0.12)
+        c.SetRightMargin(0.1)
+        prof = buildMedianProfile(h)
+        h.Draw('colz')
+        prof.Draw('e2p')
+        prof.Fit(func)
+        calibGr = prof.GetListOfFunctions().At(0).Clone(h.GetName()+title+'_calib')        
+        tex = TLatex()
+        tex.SetTextFont(42)
+        tex.SetTextSize(0.04)
+        tex.SetNDC()
+        tex.DrawLatex(0.12,0.96,'#bf{CMS} #it{simulation preliminary}')
+        tex.DrawLatex(0.15,0.88,title)
+        tex.SetTextAlign(31)
+        tex.DrawLatex(0.97,0.96,proc)        
+        c.SaveAs(os.path.join(self.outpath,h.GetName()+title+'.png'))
+        prof.Delete()        
+        c.Delete()
+        return calibGr
+
+    def _getEnergiesForCalibration(self, data, ireg):
+        """
+        Returns an array of reconstructed energies, eta and average noise
+        """
+        x = []
+        for i in range(0, data.GetEntriesFast()):
+            data.GetEntry(i)
+            genen = getattr(data,'genen')
+            geneta = abs(getattr(data,'geneta'))
+            if genen < self.mingenen: continue
+            if geneta < self.mingeneta: continue
+            if geneta > self.maxgeneta: continue
+            for il in range(1,self.nsr+1):
+                recen = getattr(data,'en_sr{}_ROI'.format(ireg,il))
+                avgnoise = ( getattr(data,'noise_sr3_layer{}'.format(il))
+                             *self.sr_area[ireg-1]/self.sr_area[2] )
+                x.append( [genen, geneta, recen, avgnoise] )
+        return np.array(x)
+
+
+    def L0L1Calibration(self, f, nq=6):
+        """
+        Performs the L0 (uniform eta response) and L1 (absolute scale) calibrations.
+        """
+        fIn = TFile.Open(f)
+        data = fIn.Get('data')
+        for ireg in [1,2,3]:
+            x = self._getEnergiesForCalibration(data, ireg)
+            xq = np.percentile(x, [i*100./nq for i in range(0,nq+1)], axis=0)
+
+            #relative calibration versus eta        
+            resVsEta = TH2F('resvseta',';|#eta|;#DeltaE/E', nq, 
+                              Carray('d',xq[:,1]), 100,-1,1)
+            for i in range(0,len(x)):
+                genEn, genEta, recEn,_ = x[i]
+                deltaE=recEn/genEn-1.
+                resVsEta.Fill(genEta,deltaE)
+            self.calib['L0'][ireg] = self._calibrateSpectrum(resVsEta,'SR%d'%ireg,
+                                                    self.plotLabel+' (PU=0)','pol2')
+    
+            #relative calibration versus energy
+            resVsEn = TH2F('resvsen',';Reconstructed energy [GeV];#DeltaE/E', nq, 
+                             Carray('d',xq[:,0]), 100,-1,1)
+            for i in range(0,len(x)):
+                genen, geneta, recen, _ = x[i]
+                recen /= (self.calib['L0'][ireg].Eval(geneta)+1.0)
+                deltaE = (recen/genen) - 1.
+                resVsEn.Fill(recen, deltaE)
+                resVsEta.Fill(genEta, deltaE)
+            self.calib['L1'][ireg] = self._calibrateSpectrum(resVsEn,'SR%d'%ireg,
+                                                self.plotLabel+' (PU=0)','pol1')
+        fIn.Close()
+
+    def PUCalibration(f, nq=10):
+        """
+        Parametrizes the absolute shift in energy as function of the 
+        average noise in the SR cone.
+        """
+        fIn=TFile.Open(f)
+        data=fIn.Get('data')
+
+        for ireg in [1,2,3]:
+            x = self._getEnergiesForCalibration(data,ireg)
+            xq=np.percentile(x, [i*100./nq for i in range(0,nq+1)], axis=0)
+
+            #relative calibration versus eta
+            resolVsNoiseFrac = TH2F('resolvsnoisefrac',';<Noise> [GeV];#DeltaE [GeV]', nq, 
+                                    Carray('d',xq[:,3]),50,-100,100)
+
+            for i in range(0,len(x)):
+                genEn,genEta,recEn,noiseEst=x[i]
+                recEn /= self.calib['L0'][ireg].Eval(geneta)+1.0
+                recEn /= self.calib['L1'][ireg].Eval(recen)+1.0
+                deltaE = recen - genen 
+                resolVsNoiseFrac.Fill(noiseEst,deltaE)
+    
+            self.calib['L2'][ireg] = self._calibrateSpectrum(resolVsNoiseFrac,'SR%d'%ireg,
+                                                    FLAGS.plotLabel+' (PU=140)', 'pol2')
+        fIn.Close()
--- HGCalMaskResolutionAna/scripts/runROICalibration.py
+++ HGCalMaskResolutionAna/scripts/runROICalibration.py
@@ -1,154 +1,32 @@
-import os, sys
-import numpy as np
+import os
 import pickle
 from UserCode.HGCalMaskResolutionAna import Argparser
-from array import array as Carray
 from collections import OrderedDict
 
-from ROOT import TCanvas, TLatex, TFile, TMath, TH1F
-from ROOT import TLegend, TH2F, TLorentzVector, TProfile, TH1D
 from ROOT import gStyle, gROOT, kTemperatureMap
-from UserCode.HGCalMaskResolutionAna.RootTools import buildMedianProfile
-
-parser = Argparser.Argparser()
-FLAGS = parser.get_flags()
-parser.print_args()
-
-A=[TMath.Pi()*1.3**2, TMath.Pi()*2.6**2, TMath.Pi()*5.3**2]
-NREG=3
-NLAYERS=28
-
-def getEnergiesForCalibration(data,ireg):
-    """
-    Returns an array of reconstructed energies, eta and average noise
-    """
-    x=[]
-    for i in range(0,data.GetEntriesFast()):
-        data.GetEntry(i)
-        genen=getattr(data,'genen')
-        geneta=abs(getattr(data,'geneta'))
-        if genen<FLAGS.mingenen: continue
-        if geneta<FLAGS.mingeneta: continue
-        if geneta>FLAGS.maxgeneta: continue
-        for il in range(1,NREG+1):
-            recen = getattr(data,'en_sr{}_ROI'.format(ireg,il))
-            avgnoise = getattr(data,'noise_sr3_layer{}'.format(il))*A[ireg-1]/A[2]
-            x.append( [genen,geneta,recen,avgnoise] )
-    return np.array(x)
-
-def calibrateSpectrum(h,title,proc,func='pol1'):
-    """
-    Calibrates a DeltaE/E versus x spectrum based on the profile.
-    """
-    c=TCanvas('c','c',500,500)
-    c.SetTopMargin(0.05)
-    c.SetBottomMargin(0.1)
-    c.SetLeftMargin(0.12)
-    c.SetRightMargin(0.1)
-    #prof=h.ProfileX()
-    prof=buildMedianProfile(h)
-    h.Draw('colz')
-    prof.Draw('e2p')
-    prof.Fit(func)
-    calibGr=prof.GetListOfFunctions().At(0).Clone(h.GetName()+title+'_calib')        
-    tex=TLatex()
-    tex.SetTextFont(42)
-    tex.SetTextSize(0.04)
-    tex.SetNDC()
-    tex.DrawLatex(0.12,0.96,'#bf{CMS} #it{simulation preliminary}')
-    tex.DrawLatex(0.15,0.88,title)
-    tex.SetTextAlign(31)
-    tex.DrawLatex(0.97,0.96,proc)        
-    c.SaveAs(os.path.join(FLAGS.outpath,h.GetName()+title+'.png'))
-    prof.Delete()        
-    c.Delete()
-    return calibGr
-
-def doL0L1Calibration(url, calib, nq=6):
-    """
-    Performs the L0 (uniform eta response) and L1 (absolute scale) calibrations.
-    """
-    #derive L0 and L1 calibrations for 3 different signal regions
-    fIn=TFile.Open(url+'.root')
-    data=fIn.Get('data')
-    for ireg in [1,2,3]:
-        x=getEnergiesForCalibration(data,ireg)
-        xq=np.percentile(x, [i*100./nq for i in range(0,nq+1)], axis=0)
-
-        #relative calibration versus eta        
-        resolVsEta = TH2F('resolvseta',';|#eta|;#DeltaE/E', nq, 
-                          Carray('d',xq[:,1]), 100,-1,1)
-        for i in range(0,len(x)):
-            genEn, genEta, recEn,_ = x[i]
-            deltaE=recEn/genEn-1.
-            resolVsEta.Fill(genEta,deltaE)
-        calib['L0'][ireg]=calibrateSpectrum(resolVsEta,'SR%d'%ireg,
-                                            FLAGS.plotLabel+' (PU=0)','pol2')
-    
-        #relative calibration versus energy
-        resolVsEn = TH2F('resolvsen',';Reconstructed energy [GeV];#DeltaE/E', nq, 
-                         Carray('d',xq[:,0]), 100,-1,1)
-        for i in range(0,len(x)):
-            genEn,genEta,recEn,_=x[i]
-            recEn=recEn/(calib['L0'][ireg].Eval(genEta)+1.0)
-            deltaE=recEn/genEn-1.
-            resolVsEn.Fill(recEn,deltaE)
-            resolVsEta.Fill(genEta,deltaE)
-        calib['L1'][ireg]=calibrateSpectrum(resolVsEn,'SR%d'%ireg,
-                                            FLAGS.plotLabel+' (PU=0)','pol1')
-    fIn.Close()
-    return calib
-
-def doPUCalibration(url, calib, nq=10):
-    """
-    Parametrizes the absolute shift in energy as function of the 
-    average noise in the SR cone.
-    """
-    fIn=TFile.Open(url+'.root')
-    data=fIn.Get('data')
-
-    for ireg in [1,2,3]:
-        x=getEnergiesForCalibration(data,ireg)
-        xq=np.percentile(x, [i*100./nq for i in range(0,nq+1)], axis=0)
-
-        #relative calibration versus eta
-        resolVsNoiseFrac = TH2F('resolvsnoisefrac',';<Noise> [GeV];#DeltaE [GeV]', nq, 
-                                Carray('d',xq[:,3]),50,-100,100)
-
-        for i in range(0,len(x)):
-            genEn,genEta,recEn,noiseEst=x[i]
-            recEn=recEn/(calib['L0'][ireg].Eval(genEta)+1.0)
-            recEn=recEn/(calib['L1'][ireg].Eval(recEn)+1.0)
-            deltaE=recEn-genEn 
-            resolVsNoiseFrac.Fill(noiseEst,deltaE)
-    
-        calib['L2'][ireg]=calibrateSpectrum(resolVsNoiseFrac,'SR%d'%ireg,
-                                            FLAGS.plotLabel+' (PU=140)', 'pol2')
-    return calib
+from UserCode.HGCalMaskResolutionAna.Calibration import Calibration
 
 def main():
+    calibration = Calibration(FLAGS.mingenen, FLAGS.mingeneta, FLAGS.maxgeneta,
+                              FLAGS.plotLabel, FLAGS.outpath)
+    calibration.L0L1Calibration(FLAGS.noPUFile)
+
+    if FLAGS.PUFile != '':
+        calibration.PUCalibration(FLAGS.PUFile)
+        with open('calib_pu{}.pck'.format(FLAGS.puTag),'w') as cachefile:
+            pickle.dump(calibration.calib, cachefile, pickle.HIGHEST_PROTOCOL)
+    else:
+        with open('calib_nopu.pck','w') as cachefile:
+            pickle.dump(calibration.calib, cachefile, pickle.HIGHEST_PROTOCOL)
+
+
+if __name__ == "__main__":
     gStyle.SetOptStat(0)
     gROOT.SetBatch(True)
     gStyle.SetPalette(kTemperatureMap)
 
-    fIn=TFile.Open(FLAGS.noPUFile+'.root')
-    data=fIn.Get('data')
+    parser = Argparser.Argparser()
+    FLAGS = parser.get_flags()
+    parser.print_args()
 
-    calib=OrderedDict()
-    calib['L0']={}
-    calib['L1']={}
-    doL0L1Calibration(url=FLAGS.noPUFile, calib=calib)
-    if FLAGS.PUFile != '':
-        calib['L2']={}
-        doPUCalibration(url=FLAGS.PUFile, calib=calib)
-        applyCalibrationTo(url=FLAGS.PUFile, calib=calib,
-                           title=FLAGS.plotLabel+' (PU={})'.format(FLAGS.puTag))
-        with open('calib_pu{}.pck'.format(FLAGS.puTag),'w') as cachefile:
-            pickle.dump(calib, cachefile, pickle.HIGHEST_PROTOCOL)
-    else:
-        with open('calib_nopu.pck','w') as cachefile:
-            pickle.dump(calib, cachefile, pickle.HIGHEST_PROTOCOL)
-
-
-if __name__ == "__main__":
     main()
