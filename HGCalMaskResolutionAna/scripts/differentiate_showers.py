d380fce5b07087213892f4eb3bcc38de4393242b
bfontana <bruno.alves@cern.ch>
2019-07-10 21:19:27 +0200

    minor update

--- HGCalMaskResolutionAna/scripts/differentiate_showers.py
+++ HGCalMaskResolutionAna/scripts/differentiate_showers.py
@@ -14,10 +14,17 @@ from UserCode.HGCalMaskResolutionAna.PartialWafersStudies import PartialWafersSt
 from UserCode.HGCalMaskVisualProd.RootPlotting import RootPlotting
 from UserCode.HGCalMaskVisualProd.RootObjects import RootHistograms
 
-def DifferentiateShowers(current, standard):
-
-
-
+def DifferentiateShowers(current, standard, threshold, min_val):
+    """Returns True if the shower is complete."""
+    assert len(current) == len(standard)
+    cumdiff = 0.
+    for i in range(len(current)):
+        if standard[i] < min_val:
+            continue
+        cumdiff += abs(standard[i] - current[i])
+    if cumdiff > threshold:
+        return False
+    return True
 
 def main():
     #gStyle.SetOptStat(0)
@@ -32,8 +39,8 @@ def main():
 
     fracEn = np.zeros((NREG,NLAYERS), dtype=float)
     countfracEn = np.zeros((NREG,NLAYERS), dtype=int)
-
-    for i in range(0,data.GetEntriesFast()):
+    nentries = data.GetEntriesFast()
+    for i in range(0,nentries):
         data.GetEntry(i)
         genen    = getattr(data,'genen')        
         geneta   = abs(getattr(data,'geneta'))
@@ -55,8 +62,8 @@ def main():
 
             for il in range(1,NLAYERS+1):
                 v = f1*getattr(data,'en_sr{}_layer{}'.format(ireg,il)) - f2
-                if ( (FLAGS.samples == "inner" and geneta < FLAGS.maxgeneta or
-                     FLAGS.samples == "outer" and geneta > FLAGS.mingeneta) and
+                if ( (FLAGS.samples == "inner" and geneta < 2.75 or
+                      FLAGS.samples == "outer" and geneta > 1.6) and
                      recen != 0 ):
                     fracEn[ireg-1,il-1] += v / recen
                     countfracEn[ireg-1,il-1] += 1
@@ -77,12 +84,10 @@ def main():
     """
 
     histos=OrderedDict()
-    etabins = 12
-    etacuts = FLAGS.etacuts
-    hn = ['fraction_complete_sr{}']
+    hn = ['fraction_complete_sr{}', 'complete_sr{}']
     for ireg in range(1,NREG+1):
-            bins = Carray('d', np.arange(2.69, 3.04, 0.01))
-            strings = ';#Eta;Fraction'
+            bins = Carray('d', np.arange(2.69, 3.04, 0.001))
+            strings = ";|#eta|;Complete showers' fraction"
             for ih in range(len(hn)):
                 histos[hn[ih].format(ireg)] = TH1F(hn[ih].format(ireg), strings,
                                                    len(bins)-1, bins)
@@ -90,8 +95,9 @@ def main():
         histos[h].Sumw2()
         histos[h].SetMarkerStyle(20)
         histos[h].SetDirectory(0)
-
-    for i in range(0,data.GetEntriesFast()):
+    it = 0
+    countzeros = np.zeros((3))
+    for i in range(0,nentries):
         data.GetEntry(i)
         genen    = getattr(data,'genen')        
         geneta   = abs(getattr(data,'geneta'))
@@ -109,38 +115,45 @@ def main():
                     if 'L2' in calib and ireg in calib['L2']:
                         f2 = calib['L2'][ireg].Eval(avgnoise)
             recen = f1*recen - f2 
-
             #differentiate complete from incomplete showers
-            ROI_en = np.zeros((NLAYERS))
+            ROI_en = np.zeros((NLAYERS), dtype=float)
             for il in range(1,NLAYERS+1):
                 v = f1*getattr(data,'en_sr{}_layer{}'.format(ireg,il)) - f2
-                ROI_en.append(v/recen)
-            DifferentiateShowers(ROI_en, fracEn[ireg-1.il-1])
-
-            quit()
-            bool_sig = ( (FLAGS.samples == 'inner' and geneta < FLAGS.maxgeneta) or 
-                         (FLAGS.samples == 'outer' and geneta >= FLAGS.mingeneta) )
-            bools_bckg = []
-            check_bools = int(bool_sig)
-            for ic in range(len(etacuts)-1):
-                check_bools += int(bools_bckg[ic])
-            assert check_bools == 1
-                
+                try:
+                    ROI_en[il-1] = v/recen
+                except ZeroDivisionError:
+                    ROI_en[il-1] = 0.
+                    countzeros[ireg-1] += 1
+            bool_sig = DifferentiateShowers(ROI_en, fracEn[ireg-1,:], 
+                                            threshold=.5, min_val=0.05)
+            if bool_sig:
+                histos[hn[0].format(ireg)].Fill(geneta)
+            histos[hn[1].format(ireg)].Fill(geneta)
+ 
     #end of tree loop    
     fIn.Close()
 
-    """
-    pcoords = [[[0.01,0.51,0.33,0.99],
-                [0.34,0.51,0.66,0.99],  
-                [0.67,0.51,0.99,0.99],
-                [0.01,0.01,0.33,0.49],   
-                [0.34,0.01,0.66,0.49],   
-                [0.67,0.01,0.99,0.49]]]
-    cdims = [[1000,600]]
-    picname = '2comp_'+FLAGS.samples
-    plotHistograms(histos, cdims, pcoords, 
-                   os.path.join(FLAGS.outpath,picname))
-    """
+    pcoords = [[[0.01,0.01,0.33,0.99],
+                [0.34,0.01,0.66,0.99],
+                [0.67,0.01,0.99,0.99]]]
+    cdims = [[2000,700]]
+    picname = 'diffshowers'    
+    correct_order = []
+    for i in range(len(hn)):
+        for ireg in range(1,NREG+1):
+            correct_order.append(hn[i].format(ireg))
+    assert len(correct_order) == len(histos.keys())
+    histos = [histos[correct_order[i]] for i in range(len(correct_order))]
+
+    for ireg in range(NREG):
+        histos[ireg].Divide(histos[ireg+NREG])
+    histos = histos[:3]
+    
+    with RootPlotting(ncanvas=1, npads=3, cdims=cdims, pcoords=pcoords) as plot:
+        for ireg in range(NREG):
+            plot.plotHistogram(cpos=0, ppos=ireg, h=histos[ireg], draw_options='HIST')
+        plot.save(cpos=0, name=picname)
+
 
 if __name__ == "__main__":
     parser = Argparser.Argparser()
