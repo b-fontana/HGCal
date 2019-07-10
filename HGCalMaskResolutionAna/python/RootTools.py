fe04bd7b1ebb58e5d3d9c1eecf6acefdeb33aaee
bfontana <bruno.alves@cern.ch>
2019-07-10 16:32:24 +0200

    minor update

--- HGCalMaskResolutionAna/python/RootTools.py
+++ HGCalMaskResolutionAna/python/RootTools.py
@@ -2,19 +2,18 @@ import ROOT
 from array import array
 
 def buildMedianProfile(h):
-
     """build the median profile"""
-
     medianGr = ROOT.TGraphAsymmErrors()
-    medianGr.SetName('%s_medianprof'%h.GetName())
+    medianGr.SetName('{}_medianprof'.format(h.GetName()))
     medianGr.SetLineWidth(2)
     medianGr.SetMarkerStyle(20)
 
-    xq = array('d', [0.16,0.5,0.84])
+    #median and 1 sigma quantiles
+    xq = array('d', [0.16,0.5,0.84]) 
+    #storage for the x values that correspond to the above quantiles
     yq = array('d', [0.0 ,0.0,0.0 ])
 
     for xbin in xrange(1,h.GetNbinsX()+1):
-
         tmp=h.ProjectionY('tmp',xbin,xbin)
         tmp.GetQuantiles(3,yq,xq)
         xcen=h.GetXaxis().GetBinCenter(xbin)
