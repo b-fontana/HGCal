c60db7fbd31859984a000d29e3e1e4d0618221c4
bfontana <bruno.alves@cern.ch>
2019-07-19 11:47:27 +0200

    restructuring; update PU calibration

--- HGCalMaskResolutionAna/python/Calibration.py
+++ HGCalMaskResolutionAna/python/Calibration.py
@@ -1,16 +1,11 @@
-import os
-import numpy as np
-from array import array as Carray
-from collections import OrderedDict
-
-from ROOT import TCanvas, TFile, TH2F, TLatex
 from UserCode.HGCalMaskResolutionAna.PartialWafersStudies import PartialWafersStudies
-from UserCode.HGCalMaskResolutionAna.RootTools import buildMedianProfile
-from UserCode.HGCalMaskVisualProd.SystemUtils import EtaStr as ES
 
 class Calibration(PartialWafersStudies, object):
     def __init__(self, mingenen, etaregions, 
                  label, samples, mask, outpath):
+        import numpy as np
+        from collections import OrderedDict
+
         super(Calibration, self).__init__()
         self.calib = OrderedDict({'L0': {}, 'L1': {}, 'L2': {}})
         self.mingenen = mingenen
@@ -21,15 +16,19 @@ class Calibration(PartialWafersStudies, object):
         self.mask = str(mask)
         self.outpath = outpath
 
-    def _CalibrateSpectrum(self, h, title, proc, func='pol1', plot=True):
+    def _calibrate_spectrum(self, h, title, proc, func='pol1', plot=True):
         """
         Calibrates a DeltaE/E versus x spectrum based on the profile.
         """
+        import os
+        from UserCode.HGCalMaskResolutionAna.RootTools import buildMedianProfile
+
         prof = buildMedianProfile(h)
         prof.Fit(func)
         calibGr = prof.GetListOfFunctions().At(0).Clone(h.GetName()+title+'_calib')
 
         if plot:
+            from ROOT import TCanvas, TLatex
             c = TCanvas('c','c',500,500)
             c.SetTopMargin(0.05)
             c.SetBottomMargin(0.1)
@@ -52,10 +51,11 @@ class Calibration(PartialWafersStudies, object):
 
         return calibGr
 
-    def _EnergiesForCalibration(self, data, ireg):
+    def _energies_for_calibration(self, data, ireg):
         """
         Returns an array of reconstructed energies, eta and average noise
         """
+        import numpy as np
         x = [[] for _ in range(len(self.etas_l))]
         for i in range(0, data.GetEntriesFast()):
             data.GetEntry(i)
@@ -78,14 +78,21 @@ class Calibration(PartialWafersStudies, object):
         return [np.array(x[i]) for i in range(len(x))]
 
 
-    def L0L1Calibration(self, f, nq=6, plot=True):
+    def nopu_calibration(self, f, nq=6, plot=True):
         """
         Performs the L0 (uniform eta response) and L1 (absolute scale) calibrations.
+        When dealing with pile-up, 'pu_calibration()' has to be called afterwords.
         """
+        import numpy as np
+        from array import array as Carray
+        from ROOT import TFile, TH2F
+        from UserCode.HGCalMaskVisualProd.SystemUtils import EtaStr as ES
+
         fIn = TFile.Open(f)
         data = fIn.Get('data')
+
         for ireg in [1,2,3]:
-            x = self._EnergiesForCalibration(data, ireg)
+            x = self._energies_for_calibration(data, ireg)
             for ieta1,ieta2 in zip(self.etas_l, self.etas_r):
                 idx = np.where(self.etas_l==ieta1)[0][0]
                 idstr = 'sr{}_from{}to{}'.format(ireg, ES(ieta1), ES(ieta2))
@@ -99,10 +106,10 @@ class Calibration(PartialWafersStudies, object):
                     genEn, genEta, recEn,_ = x[idx][i]
                     deltaE = (recEn/genEn) - 1.
                     htmp.Fill(genEta, deltaE)
-                self.calib['L0'][idstr] = self._CalibrateSpectrum(htmp,'SR%d'%ireg,
-                                                                  self.label+' (PU=0)',
-                                                                  func='pol2',
-                                                                  plot=plot)
+                self.calib['L0'][idstr] = self._calibrate_spectrum(htmp,'SR%d'%ireg,
+                                                                   self.label+' (PU=0)',
+                                                                   func='pol2',
+                                                                   plot=plot)
                 htmp.Delete()
 
                 #relative calibration versus energy
@@ -114,41 +121,57 @@ class Calibration(PartialWafersStudies, object):
                     recen /= (self.calib['L0'][idstr].Eval(geneta)+1.0)
                     deltaE = (recen/genen) - 1.
                     htmp.Fill(recen, deltaE)
-                self.calib['L1'][idstr] = self._CalibrateSpectrum(htmp,'SR%d'%ireg,
-                                                                  self.label+' (PU=0)',
-                                                                  func='pol1',
-                                                                  plot=plot)
+                self.calib['L1'][idstr] = self._calibrate_spectrum(htmp,'SR%d'%ireg,
+                                                                   self.label+' (PU=0)',
+                                                                   func='pol1',
+                                                                   plot=plot)
                 htmp.Delete()
         fIn.Close()
 
     
 
-    def PUCalibration(f, eta_region, nq=10):
+    def pu_calibration(self, f, nq=10, plot=True):
         """
-        Parametrizes the absolute shift in energy as function of the 
+        To be used in pile-up situations only.
+        It parametrizes the absolute shift in energy as function of the 
         average noise in the SR cone.
         """
+        import numpy as np
+        from array import array as Carray
+        from ROOT import TFile, TH2F
+        from UserCode.HGCalMaskVisualProd.SystemUtils import EtaStr as ES
+
         fIn=TFile.Open(f)
         data=fIn.Get('data')
 
         for ireg in [1,2,3]:
-            idstr = ( 'sr{}_from{}to{}'.format(ireg, eta_region[0], eta_region[1])
-                      if eta_region!=(0,0) else 'sr{}'.format(ireg) )
-            x = self._EnergiesForCalibration(data, ireg, eta_region)
-            xq=np.percentile(x, [i*100./nq for i in range(0,nq+1)], axis=0)
+            x = self._energies_for_calibration(data, ireg)
+            for ieta1,ieta2 in zip(self.etas_l, self.etas_r):
+                idx = np.where(self.etas_l==ieta1)[0][0]
+                idstr = 'sr{}_from{}to{}'.format(ireg, ES(ieta1), ES(ieta2))
+                xq=np.percentile(x, [i*100./nq for i in range(0,nq+1)], axis=0)
 
-            #relative calibration versus eta
-            resolVsNoiseFrac = TH2F('resVSnoisefrac_'+self.samples+'_'+self.mask+'_',
-                                    ';<Noise> [GeV];#DeltaE [GeV]', nq, 
-                                    Carray('d',xq[:,3]),50,-100,100)
-
-            for i in range(0,len(x)):
-                genEn,genEta,recEn,noiseEst=x[i]
-                recEn /= self.calib['L0'][idstr].Eval(geneta)+1.0
-                recEn /= self.calib['L1'][idstr].Eval(recen)+1.0
-                deltaE = recen - genen 
-                resolVsNoiseFrac.Fill(noiseEst,deltaE)
+                #relative calibration versus eta
+                resolVsNoiseFrac = TH2F('resVSnoisefrac_'+self.samples+'_'+self.mask+'_',
+                                        ';<Noise> [GeV];#DeltaE [GeV]', nq, 
+                                        Carray('d',xq[:,3]),50,-100,100)
+                for i in range(0,len(x)):
+                    genen, geneta, recen, noiseest = x[i]
+                    recen /= (self.calib['L0'][idstr].Eval(geneta)+1.0)
+                    recen /= (self.calib['L1'][idstr].Eval(recen)+1.0)
+                    deltaE = recen - genen 
+                    resolVsNoiseFrac.Fill(noiseEst, deltaE)
     
-            self.calib['L2'][ireg] = self._CalibrateSpectrum(resolVsNoiseFrac,'SR%d'%ireg,
-                                                    self.label+' (PU=140)', func='pol2')
+                self.calib['L2'][ireg] = self._calibrate_spectrum(resolVsNoiseFrac,
+                                                                  'SR%d'%ireg,
+                                                                  self.label+' (PU=140)', 
+                                                                  func='pol2',
+                                                                  plot=plot)
         fIn.Close()
+
+    def save(self, name):
+        if name[-4:] != '.pck':
+            raise ValueError('The calibration has to be saved as a pickle file.')
+        import pickle
+        with open(name,'w') as cachefile:
+            pickle.dump(self.calib, cachefile, pickle.HIGHEST_PROTOCOL)
--- HGCalMaskResolutionAna/scripts/runROICalibration.py
+++ HGCalMaskResolutionAna/scripts/runROICalibration.py
@@ -1,28 +1,27 @@
 import os
 import pickle
+import numpy as np
 from UserCode.HGCalMaskResolutionAna import Argparser
 from collections import OrderedDict
 
-from ROOT import gStyle, gROOT, kTemperatureMap
 from UserCode.HGCalMaskResolutionAna.Calibration import Calibration
 
 def main():
     calibration = Calibration(FLAGS.mingenen, etaregions,
                               FLAGS.plotLabel, FLAGS.samples, FLAGS.mask, FLAGS.outpath)
-    calibration.L0L1Calibration(FLAGS.noPUFile)
+    calibration.nopu_calibration(FLAGS.noPUFile, plot=False)
 
     if FLAGS.PUFile != '':
         str_pu = 'calib_'+FLAGS.samples+"_"+str(FLAGS.mask)+"_"+FLAGS.method+'_pu{}.pck'
-        calibration.PUCalibration(FLAGS.PUFile)
-        with open(calib_str_pu.format(FLAGS.puTag),'w') as cachefile:
-            pickle.dump(calibration.calib, cachefile, pickle.HIGHEST_PROTOCOL)
+        calibration.pu_calibration(FLAGS.PUFile, plot=False)
+        calibration.save(str_pu.format(FLAGS.puTag))
     else:
         str_nopu = 'calib_'+FLAGS.samples+"_"+str(FLAGS.mask)+"_"+FLAGS.method+'_nopu.pck'
-        with open(calib_str_nopu,'w') as cachefile:
-            pickle.dump(calibration.calib, cachefile, pickle.HIGHEST_PROTOCOL)
+        calibration.save(str_nopu)
 
 
 if __name__ == "__main__":
+    from ROOT import gStyle, gROOT, kTemperatureMap
     gStyle.SetOptStat(0)
     gROOT.SetBatch(True)
     gStyle.SetPalette(kTemperatureMap)
@@ -32,8 +31,13 @@ if __name__ == "__main__":
     parser.print_args()
 
     if FLAGS.method == 'fineeta':
-        etaregions = np.round(np.arange(2.7,3.031,0.001).tolist(), 3).tolist()
+        if FLAGS.samples == 'inner':
+            etaregions = np.linspace(2.7, 3.03, 331)
+        elif FLAGS.samples == 'outer':
+            etaregions = np.linspace(1.45, 1.65, 201)
     elif FLAGS.method == 'ed':
-        etaregions = [2.7, 2.94]
-
+        if FLAGS.samples == 'inner':
+            etaregions = np.array((2.7, 2.94))
+        elif FLAGS.samples == 'outer':
+            etaregions = np.array((1.55, 1.65))
     main()
